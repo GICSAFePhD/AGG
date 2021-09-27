43c4e072f907e985b2a86fa852b925211bca7aba
Martin-N-Menendez <menendez91@live.com.ar>
2021-09-27 19:58:59 -0300

    RNA in progress

--- AGG.py
+++ AGG.py
@@ -1,4 +1,5 @@
-def AGG():
-    print("#"*20+" Starting Automatic GUI Generator "+"#"*20)
-    print("Reading railML object")
-    print("Generating GUI layout")
+def AGG(test = False):
+    if test:
+        print("#"*20+" Starting Automatic GUI Generator "+"#"*20)
+        print("Reading railML object")
+        print("Generating GUI layout")
